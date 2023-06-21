447b0977e3a9995b4b62c08eaf88a7f77b3d96ec
Rudi Ziebart <99054280+Skorpion115@users.noreply.github.com>
2023-06-21 12:03:19 +0200

    Bildlauf added

---
+++ helpless-halo/src/scripts/scrolling.js
@@ -0,0 +1,28 @@
+// JavaScript-Code für automatischen Bildlauf
+const imageScroll1 = document.getElementById('imageScroll1');
+//const imageScroll2 = document.getElementById('imageScroll2');
+
+// Funktion zum automatischen Wechseln der Bilder
+function autoScroll(element) {
+  let scrollAmount = 0;
+  const scrollWidth = element.scrollWidth - element.clientWidth;
+
+  const slideTimer = setInterval(function () {
+    element.scrollTo({
+      left: scrollAmount,
+      behavior: 'smooth'
+    });
+
+    scrollAmount += element.clientWidth;
+
+    if (scrollAmount >= scrollWidth) {
+      scrollAmount = 0;
+    }
+  }, 6000);
+}
+
+// Automatischer Bildlauf beim Laden der Seite
+window.addEventListener('load', function () {
+  autoScroll(imageScroll1);
+  //autoScroll(imageScroll2);
+});
