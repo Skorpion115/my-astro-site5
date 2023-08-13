bd2f2a88f5bdbf9fef782c203bd6062889c2501f
Rudi Ziebart <99054280+Skorpion115@users.noreply.github.com>
2023-08-13 23:59:05 +0200

    preact und rss installiert

--- helpless-halo/astro.config.mjs
+++ helpless-halo/astro.config.mjs
@@ -1,46 +1,47 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    // Resolves to the "./foo" directory in your current working directory
-    root: "foo",
-    // Resolves to the "./foo/public" directory in your current working directory
-    publicDir: "public",
-    // static oder server SSR serverseitiges rändern
-    output: "static",
-    // Die endgültige Seite bei deinem Hostanbieter
-    site: "https://www.musicstudio-ziebart.de/",
-    // Sitemap intergrieren, Eine Seite aufnehmen die nicht mit Astro erstellt wurde stillgelegt!!!
-    /*
-    integrations: [sitemap({
-        customPages: ['https://www.musikstudio-ziebart.de/contact.html', 'https://www.musikstudio-ziebart.de/thanks.html'],
-        entryLimit: 10000,
-        lastmod: new Date('2022-11-24'),
-    })], */
-    // Sitemap intergrieren
-    integrations: [
-        // Beispiel: Argumente an eine Integration übergeben
-        sitemap({
-            entryLimit: 10000,
-            lastmod: new Date("2023-01-06"),
-        })
-    ],
-    // Beispiel: Erfordere abschließende Schrägstriche
-    // in Seiten-URLs während der Entwicklung
-    trailingSlash: "always",
-    build: {
+  // Resolves to the "./foo" directory in your current working directory
+  root: "foo",
+  // Resolves to the "./foo/public" directory in your current working directory
+  publicDir: "public",
+  // static oder server SSR serverseitiges rändern
+  output: "static",
+  // Die endgültige Seite bei deinem Hostanbieter
+  site: "https://www.musicstudio-ziebart.de/",
+  // Sitemap intergrieren, Eine Seite aufnehmen die nicht mit Astro erstellt wurde stillgelegt!!!
+  /*
+  integrations: [sitemap({
+      customPages: ['https://www.musikstudio-ziebart.de/contact.html', 'https://www.musikstudio-ziebart.de/thanks.html'],
+      entryLimit: 10000,
+      lastmod: new Date('2022-11-24'),
+  })], */
+  // Sitemap intergrieren
+  integrations: [
+  // Beispiel: Argumente an eine Integration übergeben
+  sitemap({
+    entryLimit: 10000,
+    lastmod: new Date("2023-01-06")
+  }), preact()],
+  // Beispiel: Erfordere abschließende Schrägstriche
+  // in Seiten-URLs während der Entwicklung
+  trailingSlash: "always",
+  build: {
     // Beispiel: Erzeuge `page.html` statt `page/index.html`
     // während des Build-Prozesses.
-        format: "directory"
-    },
-    //server: { port: 3000, host: true },
-    markdown: {
-        // Beispiel: Alle Entwürfe in den endgültigen Build einbeziehen
-        drafts: true,
-    },
-    markdown: {
-        // Beispiel: Verarbeite Markdown-Dateien ohne MDX
-        mode: "md",
-    },
+    format: "directory"
+  },
+  //server: { port: 3000, host: true },
+  markdown: {
+    // Beispiel: Alle Entwürfe in den endgültigen Build einbeziehen
+    drafts: true
+  },
+  markdown: {
+    // Beispiel: Verarbeite Markdown-Dateien ohne MDX
+    mode: "md"
+  }
 });
