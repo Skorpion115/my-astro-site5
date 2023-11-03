fa72a8c01bfc3364442c4dc7a2c0048dd13122fd
Rudi Ziebart <99054280+Skorpion115@users.noreply.github.com>
2023-11-03 14:23:14 +0100

    google.js changed

--- helpless-halo/src/scripts/google.js
+++ helpless-halo/src/scripts/google.js
@@ -2,8 +2,11 @@ window.dataLayer = window.dataLayer || [];
 function gtag() {
   dataLayer.push(arguments);
 }
+gtag("set", "allow_ad_personalization_signals", false);
 gtag("js", new Date());
 
 gtag("config", "G-CXBFDX52C7");
 
 gtag("config", "AW-93825274");
+
+gtag("config", "AW-1000620606");
