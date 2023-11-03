8bb64238456d1900bf327f288441cde07875dbf2
Rudi Ziebart <99054280+Skorpion115@users.noreply.github.com>
2023-11-03 15:37:30 +0100

    google.js auf false gestellt

--- helpless-halo/src/scripts/google.js
+++ helpless-halo/src/scripts/google.js
@@ -2,7 +2,7 @@ window.dataLayer = window.dataLayer || [];
 function gtag() {
   dataLayer.push(arguments);
 }
-gtag("set", "allow_ad_personalization_signals", true);
+gtag("set", "allow_ad_personalization_signals", false);
 gtag("js", new Date());
 
 gtag("config", "G-CXBFDX52C7");
