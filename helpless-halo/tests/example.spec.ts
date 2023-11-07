5c6d3be49f7b39b0908165f9d13781fdf01a25b6
Rudi Ziebart <99054280+Skorpion115@users.noreply.github.com>
2023-11-07 20:25:18 +0100

    example.spec.ts changed

--- helpless-halo/tests/example.spec.ts
+++ helpless-halo/tests/example.spec.ts
@@ -1,18 +1,11 @@
-import { test, expect } from '@playwright/test';
+import { expect, test, devices } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test.use(devices['iPhone 11']);
+
+test('should be titled', async ({ page, context }) => {
+  await context.route('**.jpg', route => route.abort());
   await page.goto('https://www.musicstudio-ziebart.de/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Musikunterricht/);
+  await expect(page).toHaveTitle('Musikunterricht');
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://www.musicstudio-ziebart.de/');
-
-  // Click the get started link.
-  await page.getByRole('button', { name: 'Neue Begrüßung' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Music Lesson' })).toBeVisible();
-});
