f628476d04d935854c51096e210264d6f2b5aa90
Rudi Ziebart <99054280+Skorpion115@users.noreply.github.com>
2023-11-08 21:04:35 +0100

    Neue playwright added

--- helpless-halo/tests/example.spec.ts
+++ helpless-halo/tests/example.spec.ts
@@ -1,11 +1,20 @@
-import { expect, test, devices } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
-test.use(devices['iPhone 11']);
+test('has title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
 
-test('should be titled', async ({ page, context }) => {
-  await context.route('**.jpg', route => route.abort());
-  await page.goto('https://www.musicstudio-ziebart.de/');
-
-  await expect(page).toHaveTitle('Musikunterricht');
+  // Erwarte das ein Überschrift eine zweite Überschrift hat
+  await expect(page).toHaveTitle(/Playwright/);
 });
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Klicke auf den Link Get started
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Erwarte das die Seite eine Überschrift mit den Namen Installation enthält
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
+
 
