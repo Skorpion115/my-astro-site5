6a208c1e9d5d748a2463765222751a5655c7037a
Rudi Ziebart <99054280+Skorpion115@users.noreply.github.com>
2023-11-10 10:10:24 +0100

    klavierunterricht .spec.ts changed

--- helpless-halo/tests/klavierunterricht.spec.ts
+++ helpless-halo/tests/klavierunterricht.spec.ts
@@ -6,4 +6,6 @@ test('get started link', async ({ page }) => {
   // Erwarte einen PageTitle
   await expect(page).toHaveTitle(/Klavierunterricht/);
   await expect(page.getByRole('heading', { name: 'Klavier oder Digitalpiano?' })).toBeVisible();
+  //Video
+  await page.frameLocator('iframe[title="PRAELUDIUM BMV 927 - Johann Sebastian Bach"]').locator('.ytp-cued-thumbnail-overlay-image');
 });
