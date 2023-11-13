7af9e2e994021484ef86e47e65916a9ac724f03d
Rudi Ziebart <99054280+Skorpion115@users.noreply.github.com>
2023-11-13 16:23:35 +0100

    ntelify.toml changed

--- helpless-halo/src/scripts/google.js
+++ helpless-halo/src/scripts/google.js
@@ -2,7 +2,6 @@ window.dataLayer = window.dataLayer || [];
 function gtag() {
   dataLayer.push(arguments);
 }
-gtag("set", "allow_ad_personalization_signals", false);
 gtag("js", new Date());
 
 gtag("config", "G-CXBFDX52C7");
