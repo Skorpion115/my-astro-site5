1a73002982af0370aa5a22d6690f1a4ac53d719b
Rudi Ziebart <99054280+Skorpion115@users.noreply.github.com>
2023-11-25 18:56:58 +0100

    csp changed

--- helpless-halo/tests/klavierunterricht.spec.ts
+++ helpless-halo/tests/klavierunterricht.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('get started link', async ({ page }) => {
-  await page.goto('http://localhost:4321/klavierunterricht/');
+  await page.goto('https://www.musicstudio-ziebart.de/klavierunterricht/');
 
   // Erwarte einen PageTitle
   await expect(page).toHaveTitle(/Klavierunterricht/);
