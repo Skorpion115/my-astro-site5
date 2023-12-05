7ad93a748cd15acb74b62d1ebba47fe90d912997
Rudi Ziebart <99054280+Skorpion115@users.noreply.github.com>
2023-12-05 17:20:15 +0100

    google.js changed

--- helpless-halo/src/scripts/google.js
+++ helpless-halo/src/scripts/google.js
@@ -2,8 +2,8 @@ window.dataLayer = window.dataLayer || [];
 function gtag() {
   dataLayer.push(arguments);
 }
-gtag("js", new Date());
+gtag('js', new Date());
 
-gtag("config", "GT-5TG3DK3");
+gtag('config', 'GT-5TG3DK3');
 
-gtag("config", "GT-MR4MFC6");
+gtag('config', 'GT-MR4MFC6');
