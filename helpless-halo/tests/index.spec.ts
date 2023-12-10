9a99f263ae23311fda0e8a9c4324d59b0584780e
Rudi Ziebart <99054280+Skorpion115@users.noreply.github.com>
2023-12-10 05:09:38 +0100

    csp https://www.musik-klier.de added

--- helpless-halo/tests/index.spec.ts
+++ helpless-halo/tests/index.spec.ts
@@ -43,44 +43,57 @@ test('get started link', async ({ page }) => {
 
   // Click the get started link.
   await page.getByRole('button', { name: 'Neue Begrüßung' }).click();
+
   // Überschrift
   await page.getByRole('heading', { name: 'Online-Musikunterricht' }).click();
+
   // Instrumenten Klick
-  // await page.getByRole('link', { name: 'Klavier' }).click();
+  await page.getByRole('link', { name: 'Klavier' }).click();
+  await page.goto('https://www.musicstudio-ziebart.de/');
   
   // Astro Card Formular
-  // await page.getByRole('link', { name: 'Probeunterricht→ Anmeldung zu einem kostenlosen Probeunterricht!' }).click();
+  await page.getByRole('link', { name: 'Probeunterricht→ Anmeldung zu einem kostenlosen Probeunterricht!' }).click();
+  await page.goto('https://www.musicstudio-ziebart.de/');
+
   // Link Musik Klier
-  const page1Promise = page.waitForEvent('popup');
+  // const page1Promise = page.waitForEvent('popup');
   await page.getByRole('link', { name: 'Musik Klier' }).click();
-  const page1 = await page1Promise;
+  // const page1 = await page1Promise;
+  await page.goto('https://www.musicstudio-ziebart.de/');
+
   // Max Strohmer
-  // const page2Promise = page.waitForEvent('popup');
   await page.getByRole('link', { name: 'Gitarrenbauer Max Strohmer' }).click();
-  // const page2 = await page2Promise;
+  await page.goto('https://www.musicstudio-ziebart.de/');
+
   // Link Click Mein You Tube Kanal
-  // const page3Promise = page.waitForEvent('popup');
   await page.getByRole('link', { name: 'Mein YouTube Kanal' }).click();
-  // const page3 = await page3Promise;
-  // await page.getByRole('link', { name: 'Mein YouTube Kanal' }).click();
+  await page.goto('https://www.musicstudio-ziebart.de/');
+  
   // Kurze Mitteilung
-  // await page.getByRole('link', { name: 'Nachricht→ Kurze Mitteilung an mich!' }).click();
+  await page.getByRole('link', { name: 'Nachricht→ Kurze Mitteilung an mich!' }).click();
+  await page.goto('https://www.musicstudio-ziebart.de/');
+
   // Telefon Icon
   await page.getByLabel('Telefonhörer').click();
   // Telefon Anruf
   await page.getByRole('link', { name: '+49 (0)911 6320890' }).click();
+  await page.goto('https://www.musicstudio-ziebart.de/');
+
   // E-Mail
   await page.getByRole('link', { name: 'postmaster@musicstudio-ziebart.de' }).click();
+
   // Stadtplan Ansicht
-  // const page4Promise = page.waitForEvent('popup');
   await page.getByRole('link', { name: 'Standortansicht im Stadtplan!' }).click();
-  // const page4 = await page4Promise;
+  await page.goto('https://www.musicstudio-ziebart.de/');
+
   // Astro Klick
-  // await page.getByRole('link', { name: 'Astro v3.4.2' }).click();
+  await page.getByRole('link', { name: 'Astro v3.4.2' }).click();
+  await page.goto('https://www.musicstudio-ziebart.de/');
+
   // Bild von mir
   await page.getByLabel('Bild von mir').click();
   // YouTube Button
   await page.getByRole('link', { name: 'youtube', exact: true }).click();
-  // await page.getByRole('button', { name: 'youtube' }).click();
+  // await page.getByRole('button', { name: 'youtube', exact: true }).click();
 });
 
