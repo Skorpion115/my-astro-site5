12138778a6d21fbd0f734118bb657a797e74e4f9
Rudi Ziebart <99054280+Skorpion115@users.noreply.github.com>
2024-02-07 16:07:03 +0100

    astrojs/check delete

--- helpless-halo/src/test/index.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('Metadaten sind korrekt', async ({ page }) => {
-    await page.goto("http://localhost:4321");
-  
-    await expect(page).toHaveTitle('Musikunterricht');
-  });
--- helpless-halo/tests/gitarrenunterricht.spec.ts
+++ helpless-halo/tests/gitarrenunterricht.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('get started link', async ({ page }) => {
-    await page.goto('https://www.musicstudio-ziebart.de/gitarrenunterricht/');
+    await page.goto('https://www.musicstudio-ziebart.de/gitarre/');
 
     // Erwarte einen PageTitle
     await expect(page).toHaveTitle(/Gitarrenunterricht/);
