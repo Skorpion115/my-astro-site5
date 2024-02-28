a66ccf117d75b17ba76443c61f6c10e75f96e1b9
Rudi Ziebart <99054280+Skorpion115@users.noreply.github.com>
2024-02-28 18:02:50 +0100

    google.js changed

--- helpless-halo/src/scripts/google.js
+++ helpless-halo/src/scripts/google.js
@@ -4,6 +4,16 @@ function gtag() {
 }
 gtag('js', new Date());
 
+window.gatpadsallowed = false;
+ __cmp('getVendorConsents',null,function (x,y)
+       {
+        if("customVendorConsents" in x && "s1" in x.customVendorConsents && x.customVendorConsents["s1"])
+        {
+         window.gatpadsallowed = true;
+        }
+       });
+gtag('set', 'allow_ad_personalization_signals', window.gatpadsallowed);
+
 gtag('config', 'GT-5TG3DK3');
 
 gtag('config', 'GT-MR4MFC6');
