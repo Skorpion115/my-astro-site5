d34f541fdcc1244c9888c111e594d1a3da0476ef
Rudi Ziebart <99054280+Skorpion115@users.noreply.github.com>
2024-03-05 01:10:40 +0100

    rückgängig gemacht

--- helpless-halo/src/scripts/google.js
+++ helpless-halo/src/scripts/google.js
@@ -20,6 +20,8 @@ gtag('config', 'GT-MR4MFC6');
 
 gtag('consent', 'default', {
   'ad_storage': 'denied',
+  'ad_user_data': 'denied',
+  'ad_personalization': 'denied',
   'analytics_storage': 'denied'
 });
 
