c5062ee068ae47d807c0fe1e3e3772be83d9466b
Rudi Ziebart <99054280+Skorpion115@users.noreply.github.com>
2024-05-08 08:05:14 +0200

    robotsd.txt.ts nochmals changed

--- helpless-halo/src/robots.txt.ts
+++ helpless-halo/src/robots.txt.ts
@@ -2,10 +2,7 @@ import type { APIRoute } from 'astro';
 
 const robotsTxt = `
 User-agent: *
-Disallow:
-
-User-agent: Googlebot
-Disallow:
+Allow: /
 
 Sitemap: ${new URL('sitemap-index.xml', import.meta.env.SITE).href}
 `.trim();
