3a1adf0cedda1ed4bccc4ec01bea92ab027a3185
Rudi Ziebart <99054280+Skorpion115@users.noreply.github.com>
2024-05-16 12:55:06 +0200

    ServiceWorker.js changed nach KI

--- helpless-halo/public/serviceWorker.js
+++ helpless-halo/public/serviceWorker.js
@@ -1,5 +1,6 @@
-// Globe Variablen
-const assets = [
+const cacheVersion = '_v3';
+
+const cacheAssets = [
   "/",
   "/instrumentenkauf/",
   "/blog/",
@@ -26,104 +27,40 @@ const assets = [
   "/pupils-download/",
   "/querfloetenunterricht/",
   "/klarinettenunterricht/"
+  // Füge hier die URLs für deine anderen Ressourcen hinzu...
 ];
-const cacheTypes = ["body", "font", "image"];
-const cacheVersion = "_v3";
 
-self.addEventListener("install", (event) => {
-  // waitUntil - hält den SW in installing status etwas zu machen bevor Event abgeschlossen wird
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    // Zugriff auf die Cache API im Browser um komplette Requests und Response zu speichern
-    caches
-      .open(cacheTypes[0] + cacheVersion)
-      .then((cache) => {
-        //Fügt alle Assets zum cache hinzu
-        return cache.addAll(assets);
-      })
-      //.then(self.skipWaiting())
-  );
-});
-
-function putInCache(request, response) {
-  //Console.log(request, response);
-
-  let cacheKey = cacheTypes.includes(request.destination)
-    ? request.destination
-    : "body";
-  caches.open(cacheKey + cacheVersion).then((caches) => {
-    caches.match(request, response);
-  });
-}
-
-async function cacheFirst(request) {
-    let responseFromCache = await caches.match(request);
-    if (responseFromCache) {
-      return responseFromCache;
-    }
-  
-    let responseFromNetwork = await fetch(request);
-    putInCache(request, responseFromNetwork.clone());
-    return responseFromNetwork;
-}
-
-async function networkFirst(request) {
-    try {
-      const responseFromNetwork = await fetch(request);
-      putInCache(request, responseFromNetwork.clone());
-      return responseFromNetwork;
-    } catch {
-      const responseFromCache = await caches.match(request);
-      if (responseFromCache) {
-        return responseFromCache;
-      }
-    }
-}
-
-self.addEventListener("fetch", (event) => {
-    //console.log (event.request);
-    let response = "";
-  switch (event.request.destination) {
-    case "font":
-      response = cacheFirst(event.request);
-      break;
-    case "image":
-      response = cacheFirst(event.request);
-      break;
-    default:
-      response = networkFirst(event.request);
-  }
-  event.respondWith(response);
-});
-
-// Aktivate SW
-async function deleteOldCaches() {
-  // Welche Caches sollen erhalten bleiben
-  let cacheKeepList = [];
-  cacheTypes.forEach((element) => {
-    cacheKeepList.push(element + cacheVersion);
-  });
-
-  // All caches werden ermittelt und gefiltert
-  let keyList = await caches.keys();
-  let cacheToDelete = keyList.filter((key) => !cacheKeepList.includes(key));
-
-  // Löschen der Caches welche nicht benötigt werden
-  return Promise.all(
-    cacheToDelete.map((key) => {
-      if (!cacheAllowList.includes(key)) {
-        return caches.delete(key);
-      }
+    caches.open(cacheVersion).then((cache) => {
+      return cache.addAll(cacheAssets);
     })
   );
-}
+});
 
-self.addEventListener("activate", (event) => {
-  console.log("service worker aktivated");
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request).then((response) => {
+        return caches.open(cacheVersion).then((cache) => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
   event.waitUntil(
-    deleteOldCaches().then(() => {
-      //Damit alle Clients (Tabs im Browser) den aktivierten Service Worker nutzen
-      // sonst erst nach erneutem Reload der Seite
-      clients.claim();
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((cacheName) => {
+          return cacheName !== cacheVersion;
+        }).map((cacheName) => {
+          return caches.delete(cacheName);
+        })
+      );
     })
   );
 });
