9387019969aebea6da8f6016f6a58fcc69d0a840
Rudi Ziebart <99054280+Skorpion115@users.noreply.github.com>
2024-05-20 23:33:29 +0200

    Files changed

--- helpless-halo/astro.config.mjs
+++ helpless-halo/astro.config.mjs
@@ -2,11 +2,26 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import preact from "@astrojs/preact";
 import netlify from "@astrojs/netlify";
+import vercel from "@astrojs/vercel/serverless";
+import crypto from "crypto";
 
+function nonceMiddleware(req, res, next) {
+  const nonce = crypto.randomBytes(16).toString("base64");
+  res.locals = { nonce };
+  res.setHeader(
+    "Content-Security-Policy",
+    `default-src 'self'; script-src 'self' 'nonce-${nonce}' 'unsafe-inline'`
+  );
+  next();
+}
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
   server: { port: 4321, host: true },
+
+  devToolbar: {
+    enabled: false
+  },
   
   build: {
     format: "directory", // Erzeugt `page/index.html` statt `page.html`
@@ -15,12 +30,21 @@ export default defineConfig({
   publicDir: "public",
 
   output: "server", // SSR serverseitiges Rendern
+  adapter: vercel(),
 
   adapter: netlify({
     functionPerRoute: true,
   }),
 
   vite: {
+    plugins: [
+      {
+        name: 'nonce-middleware',
+        configureServer(server) {
+          server.middlewares.use(nonceMiddleware);
+        },
+      },
+    ],
     build: {
       sourcemap: true,  // Aktiviert Source Maps in Vite
       chunkSizeWarningLimit: 3420, // Setze hier deine bevorzugte Grenze ein
