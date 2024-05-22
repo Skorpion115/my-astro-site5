02ea6cffe10fa923fc46c06a6105c5329d665318
Rudi Ziebart <99054280+Skorpion115@users.noreply.github.com>
2024-05-22 18:15:59 +0200

    alles wieder rückgängig gemacht mit ohne nounce

--- helpless-halo/astro.config.mjs
+++ helpless-halo/astro.config.mjs
@@ -2,23 +2,6 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import preact from "@astrojs/preact";
 import netlify from "@astrojs/netlify";
-import crypto from "crypto";
-
-function nonceMiddleware(req, res, next) {
-  const nonce = crypto.randomBytes(16).toString("base64");
-
-  // Ensure res.locals is an object
-  if (!res.locals) {
-    res.locals = {};
-  }
-
-  res.locals.nonce = nonce;
-  res.setHeader(
-    "Content-Security-Policy",
-    `default-src 'self'; script-src 'self' 'nonce-${nonce}'; style-src 'self' 'nonce-${nonce}'`
-  );
-  next();
-}
 
 // https://astro.build/config
 export default defineConfig({
@@ -26,9 +9,9 @@ export default defineConfig({
   server: { port: 4321, host: true },
 
   devToolbar: {
-    enabled: false
+    enabled: false,
   },
-  
+
   build: {
     format: "directory", // Erzeugt `page/index.html` statt `page.html`
   },
@@ -40,20 +23,11 @@ export default defineConfig({
     functionPerRoute: true,
   }),
 
-  vite: {
-    plugins: [
-      {
-        name: 'nonce-middleware',
-        configureServer(server) {
-          server.middlewares.use(nonceMiddleware);
-        },
-      },
-    ],
-    build: {
-      sourcemap: true,  // Aktiviert Source Maps in Vite
-      chunkSizeWarningLimit: 3420, // Setze hier deine bevorzugte Grenze ein
-    },
+  build: {
+    sourcemap: true, // Aktiviert Source Maps in Vite
+    chunkSizeWarningLimit: 3420, // Setze hier deine bevorzugte Grenze ein
   },
+
   site: "https://www.musicstudio-ziebart.de/",
   integrations: [
     sitemap({
