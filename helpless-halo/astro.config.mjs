2237351832e98d6ee78a9ca1b3164059f8b43322
Rudi Ziebart <99054280+Skorpion115@users.noreply.github.com>
2024-05-22 17:27:03 +0200

    1a7e9026

--- helpless-halo/astro.config.mjs
+++ helpless-halo/astro.config.mjs
@@ -2,11 +2,32 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import preact from "@astrojs/preact";
 import netlify from "@astrojs/netlify";
+import crypto from "crypto";
+
+function nonceMiddleware(req, res, next) {
+  const nonce = crypto.randomBytes(16).toString("base64");
+
+  // Ensure res.locals is an object
+  if (!res.locals) {
+    res.locals = {};
+  }
+
+  res.locals.nonce = nonce;
+  res.setHeader(
+    "Content-Security-Policy",
+    `default-src 'self'; script-src 'self' 'nonce-${nonce}'; style-src 'self' 'nonce-${nonce}'`
+  );
+  next();
+}
 
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
   server: { port: 4321, host: true },
+
+  devToolbar: {
+    enabled: false
+  },
   
   build: {
     format: "directory", // Erzeugt `page/index.html` statt `page.html`
@@ -15,12 +36,19 @@ export default defineConfig({
   publicDir: "public",
 
   output: "server", // SSR serverseitiges Rendern
-
   adapter: netlify({
     functionPerRoute: true,
   }),
 
   vite: {
+    plugins: [
+      {
+        name: 'nonce-middleware',
+        configureServer(server) {
+          server.middlewares.use(nonceMiddleware);
+        },
+      },
+    ],
     build: {
       sourcemap: true,  // Aktiviert Source Maps in Vite
       chunkSizeWarningLimit: 3420, // Setze hier deine bevorzugte Grenze ein
