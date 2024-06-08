7fd2ea302482e308b138af3ee18d86849a2c595c
Rudi Ziebart <99054280+Skorpion115@users.noreply.github.com>
2024-06-08 21:29:10 +0200

    datenschutz.astro changed

--- helpless-halo/public/scripts/consentmanager2.js
+++ helpless-halo/public/scripts/consentmanager2.js
@@ -1,7 +1,7 @@
-// content of consentmanager.js
+// content of consentmanager2.js
 document.addEventListener("DOMContentLoaded", function() {
     const script = document.createElement('script');
-    script.src = "https://delivery.consentmanager.net/delivery/vendorlist.php?cdid=b018a967f10b6";
+    script.src = "https://delivery.consentmanager.net/delivery/cookieinfo.php?cdid=b018a967f10b6";
     script.async = true;
     document.head.appendChild(script);
 });
