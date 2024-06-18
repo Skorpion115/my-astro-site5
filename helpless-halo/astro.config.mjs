0f35bf573c2f6f724ec44a99b2cbf9cacb293e1d
Rudi Ziebart <99054280+Skorpion115@users.noreply.github.com>
2024-06-18 18:13:11 +0200

    Versuch auf first-party-cookies umzustellen

--- helpless-halo/astro.config.mjs
+++ helpless-halo/astro.config.mjs
@@ -4,15 +4,39 @@ import preact from "@astrojs/preact";
 import netlify from "@astrojs/netlify";
 import { fileURLToPath } from "url";
 import { dirname, resolve } from "path";
+import cookieParser from "cookie-parser";
 
 // Convert import.meta.url to __dirname and __filename
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+// Middleware function to handle cookies
+const cookieMiddleware = (req, res, next) => {
+  cookieParser()(req, res, () => {
+    if (!req.cookies.userConsent) {
+      // Set a default cookie if not present with SameSite=None and Secure attributes
+      res.cookie('userConsent', 'false', { 
+        maxAge: 900000, 
+        httpOnly: true, 
+        sameSite: 'None', 
+        secure: true 
+      });
+    }
+    next();
+  });
+};
+
+
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
-  server: { port: 4321, host: true },
+  server: { 
+    port: 4321, 
+    host: true,
+    middleware: [
+      cookieMiddleware
+    ] 
+  },
 
   devToolbar: {
     enabled: false,
