c52b533798de2703a4389ff5379efcf7aa5eb13b
Rudi Ziebart <99054280+Skorpion115@users.noreply.github.com>
2024-06-19 12:46:31 +0200

    BaseLayout changed

---
+++ helpless-halo/public/scripts/gtag.js
@@ -0,0 +1,6 @@
+// gtag.js
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag("js", new Date());
+
+gtag("config", "GT-5TG3DK3");
---
+++ helpless-halo/public/scripts/gtag3.js
@@ -0,0 +1,6 @@
+// gtag3.js
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag("js", new Date());
+
+gtag("config", "GT-MR4MFC6");
