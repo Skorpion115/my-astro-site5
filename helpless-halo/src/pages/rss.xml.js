7e6beefbaf2a9024891489135e81b759e3f68dd0
Rudi Ziebart <99054280+Skorpion115@users.noreply.github.com>
2024-06-30 11:25:35 +0200

    netlify changed

--- helpless-halo/src/pages/rss.xml.js
+++ helpless-halo/src/pages/rss.xml.js
@@ -5,7 +5,7 @@ export async function GET() {
     title: 'Musicstudio Ziebart | Blog',
     description: 'Meine Reise zum Erlernen von Astro.',
     site: 'https://www.musicstudio-ziebart.de',
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    items: await pagesGlobToRssItems(import.meta.glob('./**/*.mdx')),
     customData: `<language>de</language>`,
   });
 }
