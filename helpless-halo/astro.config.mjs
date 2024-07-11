0fb8d8e0656a7158ff155a145619354af3ccebf3
Rudi Ziebart <99054280+Skorpion115@users.noreply.github.com>
2024-07-11 12:28:40 +0200

    astro.config.mjs changed

--- helpless-halo/astro.config.mjs
+++ helpless-halo/astro.config.mjs
@@ -1,4 +1,36 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+
+    devToolbar: {
+        enabled: false,
+    },
+
+    build: {
+        format: "directory", // Erzeugt `page/index.html` statt `page.html`
+        sourcemap: true, // Aktiviert Source Maps in Vite
+        chunkSizeWarningLimit: 3420 // Setze hier deine bevorzugte Grenze ein
+    },
+
+    output: "hybrid", // Statisches und serverseitiges Rendern
+    adapter: netlify({
+        functionPerRoute: true, // Falls du Route-spezifische Funktionen verwenden möchtest
+  }),
+
+  // Project root directory
+  root: ".",
+
+  outDir: "dist", // Ensure this is correctly set
+
+  publicDir: "public",
+
+  site: "https://www.musicstudio-ziebart.de",
+
+  trailingSlash: "always",
+  markdown: {
+    drafts: true,
+    mode: "md",
+  },
+});
