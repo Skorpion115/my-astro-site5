c89a0774f49e24c42e0172b61fd880338f554da4
Rudi Ziebart <99054280+Skorpion115@users.noreply.github.com>
2024-07-13 10:43:48 +0200

    serviceWorker installiert

---
+++ helpless-halo/public/serviceWorker.js
@@ -0,0 +1,74 @@
+const cacheVersion = "_v3";
+
+const cacheAssets = [
+  "/",
+  "/instrumentenkauf/",
+  "/contact_2/",
+  "/thankyou/",
+  "/danke-seite/",
+  "/contact_4/",
+  "/honorar/",
+  "/unterricht/",
+  "/faq/",
+  "/download/",
+  "/harmonielehre/",
+  "/impressum/",
+  "/klavierunterricht/",
+  "/keyboardunterricht/",
+  "/gitarrenunterricht/",
+  "/e-bassunterricht/",
+  "/e-gitarrenunterricht/",
+  "/banjounterricht/",
+  "/akkordeonunterricht/",
+  "/saxophonunterricht/",
+  "/pupils-download/",
+  "/querfloetenunterricht/",
+  "/klarinettenunterricht/",
+  // Füge hier die URLs für deine anderen Ressourcen hinzu...
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(cacheVersion).then((cache) => {
+      return cache.addAll(cacheAssets);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse && event.request.method === "GET") {
+        return cachedResponse;
+      } else {
+        return fetch(event.request).then((response) => {
+          if (event.request.method === "GET") {
+            return caches.open(cacheVersion).then((cache) => {
+              cache.put(event.request, response.clone());
+              return response;
+            });
+          } else {
+            return response;
+          }
+        });
+      }
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("service worker aktivated");
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((cacheName) => {
+            return cacheName !== cacheVersion;
+          })
+          .map((cacheName) => {
+            return caches.delete(cacheName);
+          })
+      );
+    })
+  );
+});
