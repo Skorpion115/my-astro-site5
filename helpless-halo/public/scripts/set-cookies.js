b9ce86fb1e683e16ec28c4e779729f09cee8f427
Rudi Ziebart <99054280+Skorpion115@users.noreply.github.com>
2024-07-15 18:02:45 +0200

    set-cookies.js wieder neu

--- helpless-halo/public/scripts/set-cookies.js
+++ helpless-halo/public/scripts/set-cookies.js
@@ -1,10 +1,10 @@
-document.cookie = "IDE=AHWqTUkRqm0pAd2QRa0nbJcDKs-QA7zIrUdUHLrTnkbBTjLnpp6DFylm6FLWqX66; SameSite=None; Secure; domain=.doubleclick.net; path=/; expires=Fri, 09 Aug 2025 15:44:03 GMT";
-document.cookie = "__Secure-3PAPISID=iYiKopY2ObXnYZ_W/A2OHIPmMct8llNnTQ; SameSite=None; Secure; domain=.google.de; path=/; expires=Fri, 19 Aug 2025 15:43:41 GMT";
-document.cookie = "__Secure-3PAPISID=iYiKopY2ObXnYZ_W/A2OHIPmMct8llNnTQ; SameSite=None; Secure; domain=.google.com; path=/; expires=Fri, 19 Aug 2025 15:43:41 GMT";
-document.cookie = "__Secure-3PSID=g.a000lwjQLHM791u5m6-NpzQQVx9iNpM1GuYdfEl1mdVaFXB2EkccVb8ApJXQNArdVCW2z2Em1QACgYKAUMSARcSFQHGX2Mi55cM74EjJTf39z-mBrZw1hoVAUF8yKqSsNNJAl4Ihotpizsv-7680076; SameSite=None; Secure; domain=.google.de; path=/; expires=Fri, 19 Aug 2025 15:43:41 GMT";
-document.cookie = "__Secure-3PSID=g.a000lwjQLHM791u5m6-NpzQQVx9iNpM1GuYdfEl1mdVaFXB2EkccVb8ApJXQNArdVCW2z2Em1QACgYKAUMSARcSFQHGX2Mi55cM74EjJTf39z-mBrZw1hoVAUF8yKqSsNNJAl4Ihotpizsv-7680076; SameSite=None; Secure; domain=.google.com; path=/; expires=Fri, 19 Aug 2025 15:43:41 GMT";
-document.cookie = "__Secure-3PSIDCC=AKEyXzUg8BOVWMQM7NTROFYpKuX6_S6ovnGAoMuQti_Yf53rqEi8MKrlYVaZlQ8t8QuEU5NtZw; SameSite=None; Secure; domain=.google.com; path=/; expires=Tue, 15 Jul 2025 15:44:03 GMT";
-document.cookie = "__Secure-3PSIDTS=sidts-CjIB4E2dkaEZRu3XJPfRGFd9f8EaPa31IuY-xv0H7p6dLc8Zgl0psF0M1l5euvG5kJIVqBAA; SameSite=None; Secure; domain=.google.com; path=/; expires=Fri, 19 Aug 2025 15:43:41 GMT";
+document.cookie = "IDE=value; SameSite=None; Secure; domain=.doubleclick.net; path=/; expires=Fri, 09 Aug 2025 15:44:03 GMT";
+document.cookie = "__Secure-3PAPISID=value; SameSite=None; Secure; domain=.google.de; path=/; expires=Tue, 19 Aug 2025 15:43:41 GMT";
+document.cookie = "__Secure-3PAPISID=value; SameSite=None; Secure; domain=.google.com; path=/; expires=Tue, 19 Aug 2025 15:43:41 GMT";
+document.cookie = "__Secure-3PSID=value; SameSite=None; Secure; domain=.google.de; path=/; expires=Tue, 19 Aug 2025 15:43:41 GMT";
+document.cookie = "__Secure-3PSID=value; SameSite=None; Secure; domain=.google.com; path=/; expires=Tue, 19 Aug 2025 15:43:41 GMT";
+document.cookie = "__Secure-3PSIDCC=value; SameSite=None; Secure; domain=.google.com; path=/; expires=Tue, 15 Jul 2025 15:44:03 GMT";
+document.cookie = "__Secure-3PSIDTS=value; SameSite=None; Secure; domain=.google.com; path=/; expires=Tue, 19 Aug 2025 15:43:41 GMT";
 
 
 
