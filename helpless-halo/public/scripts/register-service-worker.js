6def146e8f6e3dbc4d26a45bf83adc6c16aef98d
Rudi Ziebart <99054280+Skorpion115@users.noreply.github.com>
2024-08-10 12:23:47 +0200

    register-service-worker.js changed

--- helpless-halo/public/scripts/register-service-worker.js
+++ helpless-halo/public/scripts/register-service-worker.js
@@ -1,3 +1,4 @@
+/*
 document.addEventListener("DOMContentLoaded", () => {
     if ("serviceWorker" in navigator) {
       navigator.serviceWorker
@@ -5,4 +6,20 @@ document.addEventListener("DOMContentLoaded", () => {
         .then(() => console.log("serviceWorker registered"))
         .catch((error) => console.warn("ServiceWorker registration failed:", error));
     }
+  }); */
+
+  document.addEventListener("DOMContentLoaded", () => {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker
+        .register("/serviceWorker.js", { scope: "/" })
+        .then((registration) => {
+          console.log("Service Worker registered with scope:", registration.scope);
+        })
+        .catch((error) => {
+          console.error("Service Worker registration failed:", error);
+        });
+    } else {
+      console.warn("Service Worker not supported in this browser.");
+    }
   });
+  
