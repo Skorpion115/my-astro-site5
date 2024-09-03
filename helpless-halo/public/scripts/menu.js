be15befb7abc326d99a55d3f9f65003253b38f9e
Rudi Ziebart <99054280+Skorpion115@users.noreply.github.com>
2024-09-03 20:49:19 +0200

    menu.js changed

--- helpless-halo/public/scripts/menu.js
+++ helpless-halo/public/scripts/menu.js
@@ -5,7 +5,18 @@ document.addEventListener("astro:page-load", () => {
   if (hamburger) {
     hamburger.addEventListener("click", () => {
       navLinks.classList.toggle("expanded");
-      hamburger.classList.toggle("active"); // Toggle der aktiven Klasse
+      hamburger.classList.toggle("active");
+      document.body.classList.toggle("menu-open");
     });
   }
+
+  // Menü schließen, wenn man außerhalb des Menüs klickt
+  document.addEventListener("click", (event) => {
+    const isClickInside = navLinks.contains(event.target) || hamburger.contains(event.target);
+    if (!isClickInside) {
+      navLinks.classList.remove("expanded");
+      hamburger.classList.remove("active");
+      document.body.classList.remove("menu-open");
+    }
+  });
 });
