dacdd551a390d1a3c8f90ed2f6ea9794df4daaba
Rudi Ziebart <99054280+Skorpion115@users.noreply.github.com>
2024-09-03 05:33:41 +0200

    nav-links-secondary nav leiste erstellt

--- helpless-halo/public/scripts/menu.js
+++ helpless-halo/public/scripts/menu.js
@@ -1,11 +1,11 @@
-document.addEventListener('astro:page-load', () => {
-  const hamburger = document.querySelector('.hamburger');
-  const navLinks = document.querySelector('.nav-links');
+document.addEventListener("astro:page-load", () => {
+  const hamburger = document.querySelector(".hamburger");
+  const navLinks = document.querySelector(".nav-links");
 
   if (hamburger) {
-    hamburger.addEventListener('click', () => {
-      navLinks.classList.toggle('expanded');
-      hamburger.classList.toggle('active'); // Toggle der aktiven Klasse
+    hamburger.addEventListener("click", () => {
+      navLinks.classList.toggle("expanded");
+      hamburger.classList.toggle("active"); // Toggle der aktiven Klasse
     });
   }
 });
---
+++ helpless-halo/public/scripts/secondary-menu.js
@@ -0,0 +1,9 @@
+document.addEventListener("astro:page-load", () => {
+  document
+    .querySelector(".hamburger-secondary")
+    .addEventListener("click", function () {
+      document
+        .querySelector(".nav-links-secondary")
+        .classList.toggle("expanded");
+    });
+});
