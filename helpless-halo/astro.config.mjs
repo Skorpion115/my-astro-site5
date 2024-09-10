e011888e52ea253e9b45ed8c1b609c15fa228fe2
Rudi Ziebart <99054280+Skorpion115@users.noreply.github.com>
2024-09-10 17:21:39 +0200

    astro.config.mjs changed

--- helpless-halo/astro.config.mjs
+++ helpless-halo/astro.config.mjs
@@ -17,6 +17,8 @@ export default defineConfig({
   build: {
     format: "directory", // Erzeugt `page/index.html` statt `page.html`
     sourcemap: true, // Aktiviert Source Maps in Vite
+    inlineStylesheets: `never`, /* Projektstile werden in externen Stylesheets gesendet */
+    serverEntry: "main.mjs", /* damit die runtime erkennt, dass es sich bei der Datei um ein JavaScript-Modul handelt..mjs */
     chunkSizeWarningLimit: 3420 // Setze hier deine bevorzugte Grenze ein
   },
   
