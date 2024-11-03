8634120129a4fe60058d2ab39489882544b63bcf
Rudi Ziebart <99054280+Skorpion115@users.noreply.github.com>
2024-11-03 10:45:59 +0100

    serviceWorker.js changed

--- helpless-halo/public/serviceWorker.js
+++ helpless-halo/public/serviceWorker.js
@@ -26,6 +26,7 @@ const cacheAssets = [
   "/banjounterricht/",
   "/banjo-videoanleitung/",
   "/akkordeonunterricht/",
+  "/akkordeon-videoanleitung/",
   "/saxophonunterricht/",
   "/pupils-download/",
   "/querfloetenunterricht/",
